4511096747f88b248e9eb16a89c7469716ef50a0
shozawa <tomohiro.shozawa@gmail.com>
2019-04-27 08:30:04 +0900

    improve ast
    add Program, LetStatement, Identifier.

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,9 @@
 package ast
 
+import (
+	"github.com/shozawa/monkey/token"
+)
+
 type Node interface {
 	TokenLiteral() string
 }
@@ -12,4 +16,37 @@ type Statement interface {
 type Expression interface {
 	Node
 	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (l *LetStatement) statementNode() {}
+func (l *LetStatement) TokenLiteral() string {
+	return l.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
 }
