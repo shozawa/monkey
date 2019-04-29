8e3134a85d7adbb6a239bb6b643ff5e666cb43b1
shozawa <tomohiro.shozawa@gmail.com>
2019-04-29 09:30:07 +0900

    improve repl

--- repl/repl.go
+++ repl/repl.go
@@ -5,14 +5,17 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/shozawa/monkey/evaluator"
 	"github.com/shozawa/monkey/lexer"
-	"github.com/shozawa/monkey/token"
+	"github.com/shozawa/monkey/object"
+	"github.com/shozawa/monkey/parser"
 )
 
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnv()
 	for {
 		fmt.Printf(PROMPT)
 		scanned := scanner.Scan()
@@ -21,13 +24,14 @@ func Start(in io.Reader, out io.Writer) {
 		}
 		line := scanner.Text()
 		l := lexer.New(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			if tok.Type == token.ILLEGAL {
-				// TODO: Error handling
-				fmt.Println("Parse Error.")
-				break
-			}
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(l)
+		program := p.Parse()
+		obj := evaluator.Eval(&program, env)
+		if obj != nil {
+			fmt.Printf("%q\n", obj.Inspect())
+		} else {
+			// FIXME: print correct value
+			fmt.Print("nil\n")
 		}
 	}
 }
