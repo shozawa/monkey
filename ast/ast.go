d7a3f4ecbdc8f576b55ca435d79f717c00c60e2b
shozawa <tomohiro.shozawa@gmail.com>
2019-05-01 12:00:31 +0900

    add ast.CallExpression

--- ast/ast.go
+++ ast/ast.go
@@ -163,3 +163,17 @@ func (f *FunctionLiteral) TokenLiteral() string {
 func (f *FunctionLiteral) String() string {
 	return "TODO"
 }
+
+type CallExpression struct {
+	Token     token.Token
+	Function  Expression
+	Arguments []Expression
+}
+
+func (c *CallExpression) expressionNode() {}
+func (c *CallExpression) TokenLiteral() string {
+	return c.Token.Literal
+}
+func (c *CallExpression) String() string {
+	return "TODO"
+}
